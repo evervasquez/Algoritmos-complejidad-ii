0d971e6075ddeca72199ff27b9352abda6bceb92
eveR Vasquez <pever@unsm.edu.pe>
2016-01-22 10:47:46 -0500

    clase junary 22, 2016

---
+++ AreaRectangulo/src/arearectangulo/AreaRectangulo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arearectangulo;
+
+/**
+ *
+ * @author eveR
+ */
+public class AreaRectangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declarar variables
+        double area, altura, base;
+        
+        //instanciar variables
+        base = 12;
+        altura = 3;
+        
+        //operación
+        area = base * altura;
+        
+        System.out.println("El area es "+ area);
+    }
+    
+}
